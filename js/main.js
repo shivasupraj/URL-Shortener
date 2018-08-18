c30f361b826b01b5824953787592f08fa1652f9b
shivasupraj <shivasupraj.p@gmail.com>
2018-08-18 12:42:05 -0500

    added the popover messages

--- js/main.js
+++ js/main.js
@@ -1,12 +1,23 @@
 var end_point = "https://www.jsonstore.io/461a1a6b80cc1c44fec94f407eb347d20f508770575f5e5c2f6f472ed4208006";
 
+/***
+checks if the input bar is not empty
+and displays a message
+***/
+$(document).ready(function(){
+   $('.btn').popover({placement: "bottom", trigger:"focus", container:"body",  content:function() { if(document.getElementById("urlinput").value !== "") {return "Link copied to the clipboard"} return "Enter a valid url"  }  });
+});
+
 pathname = window.location.href;
 path = pathname.split('/').pop();
 path = pathname.split('#')[1];
-console.log(path);
 
+/***
+parses the urls and takes the hash value
+retreives the url associated with the value
+stored at the end point.
+**/
 if (path !== "") {
-    console.log(path)
     $.getJSON(end_point + "/" + path, function (data) {
         data = data["result"];
         if (data != null) {
@@ -17,6 +28,10 @@ if (path !== "") {
     });
 }
 
+/***
+generates a random hash value
+to stored as a key in then JSON file
+***/
 var hash = getrandom();
 function getrandom() {
     var text = "";
@@ -51,9 +66,11 @@ function send_request(url) {
 }
 
 function shorturl(){
-    var longurl = geturl();
-    send_request(longurl);
-    document.getElementById('id').text = window.location.href + '#' + hash;
-    document.getElementById('id').href = window.location.href + '#' + hash;
-    simplecopy(document.getElementById('id').text);
+    if(document.getElementById("urlinput").value !== ""){
+      var longurl = geturl();
+      send_request(longurl);
+      document.getElementById('id').text = window.location.href + '#' + hash;
+      document.getElementById('id').href = window.location.href + '#' + hash;
+      simplecopy(document.getElementById('id').text);
+    }
 }
